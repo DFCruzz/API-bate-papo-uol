2c261218f3b972df909da1dd4a09db5b02f977ae
DFCruzz <diegofcruzz@gmail.com>
2023-01-13 17:02:56 -0300

    get and psot messages function added

--- src/app.js
+++ src/app.js
@@ -24,6 +24,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+let time = dayjs()
+
 app.get("/participants", (req, res) => {
     database.collection("participants")
         .find()
@@ -52,6 +54,14 @@ app.post("/participants", async (req, res) => {
             lastStatus: Date.now()
         })
 
+        await database.collection("messages").insertOne({
+            from: name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: time.format("HH:mm:ss")
+        })
+
         res.sendStatus(201)
     }
 
@@ -60,9 +70,51 @@ app.post("/participants", async (req, res) => {
     }
 })
 
+app.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body
+    const { loggedUser } = req.headers
+
+    try {
+        const isUserLogged = await database.collection("messages").findOne({ name: loggedUser})
+
+        if(!isUserLogged) {
+            return res.status(422).send("Usuário desconectado!")
+        }
+
+        await database.collection("messages").insertOne({
+            from: loggedUser,
+            to: to,
+            text: text,
+            type: type,
+            time: time.format("HH:mm:ss")
+        })
+
+        res.sendStatus(201)
+    }
+
+    catch (error) {
+        res.status(500).send(error.message)        
+    }
+})
+
+app.get("/messages", async (req, res) => {
+    try {
+        database.collection("messages")
+        .find()
+        .toArray()
+        .then(data => {
+            return res.send(data)
+        })
+    }
+
+    catch {
+        res.sendStatus(500)
+    }
+})
 
 
-const PORT = process.env.PORT
+
+const PORT = 5000
 app.listen( PORT, () => {
     console.log(`Servidor iniciado na porta: ${PORT}`)
 })
